b367fafa3e9805955738eeb604f5f8ccc65eaeec
Ivan Ivanov <ivanov.ivan.v@gmail.com>
2022-08-20 01:49:06 +0300

    feat: add new Select component

---
+++ src/common/components/Select.js
@@ -0,0 +1,21 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+const Select = ({ options }) => (
+  <>
+    <label htmlFor="chartsSelection">Please, select a chart</label>
+    <select name="chartsSelection">
+      {options.map((o) => (
+        <option value={o.value}>{o.label}</option>
+      ))}
+    </select>
+  </>
+);
+
+Select.propTypes = {
+  options: PropTypes.arrayOf(
+    PropTypes.shape({ value: PropTypes.string, label: PropTypes.string })
+  ),
+};
+
+export default Select;
