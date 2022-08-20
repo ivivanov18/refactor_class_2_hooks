f28d13db31d83d3c7dcc0021d8d8665f98d1f4c3
Ivan Ivanov <ivanov.ivan.v@gmail.com>
2022-08-20 15:33:04 +0300

    feat: replace DashboardShell in App by SelectContainer

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React from "react";
 import DashboardShell from "./features/Dashboard/DashboardShell";
+import SelectContainer from "./common/components/SelectContainer";
 
 const App = () => {
-  return <DashboardShell />;
+  return <SelectContainer />;
 };
 
 export default App;
