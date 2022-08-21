e6f8e3c5bb73bfbc10f9e220a8b67e1b6da6f077
Ivan Ivanov <ivanov.ivan.v@gmail.com>
2022-08-22 01:00:48 +0300

    refactor: - refactor DashboardShell component from class component to functional component using hooks - TODO: componentDidMount and handleSelectChange

--- src/features/Dashboard/DashboardShell.js
+++ src/features/Dashboard/DashboardShell.js
@@ -1,51 +1,43 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import Aside from "../../common/components/Aside";
 import ChartContainer from "./ChartContainer";
 import Layout from "../../common/components/Layout";
 import Main from "../../common/components/Main";
 import SummaryContainer from "./SummaryContainer";
-import { connect } from "react-redux";
-import { fetchDataset } from "./DashboardSlice";
+import Select from "../../common/components/Select";
+// import { fetchDataset } from "./DashboardSlice";
 
-class DashboardShell extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { selectedLabel: "" };
+function DashboardShell() {
+  const [selectedLabel, setSelectedLabel] = useState("");
 
-    this.handleSelectChange = this.handleSelectChange.bind(this);
-  }
+  // componentDidMount() {
+  //   this.props.fetchDataset(`${process.env.REACT_APP_BASE_URL}/totals/`);
+  // }
 
-  componentDidMount() {
-    this.props.fetchDataset(`${process.env.REACT_APP_BASE_URL}/totals/`);
-  }
+  const handleSelectChange = (event) => {
+    // const selectedLabel = event.target.selectedOptions[0].label;
+    // this.props.fetchDataset(event.target.value);
+    // this.setState({ selectedLabel });
+  };
 
-  handleSelectChange(event) {
-    const selectedLabel = event.target.selectedOptions[0].label;
-    this.props.fetchDataset(event.target.value);
-    this.setState({ selectedLabel });
-  }
-
-  buildSelect() {
+  const buildSelect = () => {
     const optionsForSelect = [
-      { label: "Sales", value: `${process.env.REACT_APP_BASE_URL}/sales/` },
+      { label: "Sales", value: "sales" },
       {
         label: "Subscriptions",
-        value: `${process.env.REACT_APP_BASE_URL}/subscriptions/`
-      }
+        value: "subscriptions",
+      },
     ];
 
     return (
       <>
-        <label htmlFor="select-product">Please select a chart:</label>
         <div className="field">
-          <select id="select-product" onChange={this.handleSelectChange}>
-            <option value="">--</option>
-            {optionsForSelect.map(option => (
-              <option key={option.label} value={option.value}>
-                {option.label}
-              </option>
-            ))}
-          </select>
+          <Select
+            id="select-product"
+            label="Please select a chart:"
+            onChange={handleSelectChange}
+            options={optionsForSelect}
+          />
           <div className="chevron-wrapper flex">
             <svg
               className="chevron"
@@ -58,29 +50,23 @@ class DashboardShell extends Component {
         </div>
       </>
     );
-  }
+  };
 
-  render() {
-    return (
-      <Layout>
-        <Aside>
-          <h2># Polly dashboard</h2>
-          {this.buildSelect()}
-        </Aside>
-        <Main>
-          <h1>
-            Welcome, <span className="bold">learner!</span>
-          </h1>
-          <SummaryContainer />
-          <ChartContainer selectedLabel={this.state.selectedLabel} />
-        </Main>
-      </Layout>
-    );
-  }
+  return (
+    <Layout>
+      <Aside>
+        <h2># Polly dashboard</h2>
+        {buildSelect()}
+      </Aside>
+      <Main>
+        <h1>
+          Welcome, <span className="bold">learner!</span>
+        </h1>
+        <SummaryContainer />
+        <ChartContainer selectedLabel={selectedLabel} />
+      </Main>
+    </Layout>
+  );
 }
 
-const mapDispatchToProps = {
-  fetchDataset
-};
-
-export default connect(null, mapDispatchToProps)(DashboardShell);
+export default DashboardShell;
