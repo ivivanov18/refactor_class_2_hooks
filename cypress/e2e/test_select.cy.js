cfb616785453009c3f35fa8c7eaf4c23a4dbd955
Ivan Ivanov <ivanov.ivan.v@gmail.com>
2022-08-23 18:01:42 +0300

    tests: - add tests for select sales and subscriptions options

--- cypress/e2e/test_select.cy.js
+++ cypress/e2e/test_select.cy.js
@@ -3,7 +3,14 @@
 describe("Select different options", () => {
   beforeEach(() => {
     cy.visit("http://localhost:3000/");
-    cy.intercept("GET", "/api/sales/", {
+  });
+
+  it("should have `sales` option selected by default", () => {
+    cy.get("select").should("have.value", "sales");
+  });
+
+  it("should select Sales", () => {
+    cy.intercept("GET", "/api/sales", {
       statusCode: 200,
       body: [
         {
@@ -16,19 +23,31 @@ describe("Select different options", () => {
         },
       ],
     }).as("getSales");
-  });
-
-  it("should have `sales` option selected by default", () => {
-    cy.get("select").should("have.value", "sales");
-  });
-
-  it("should select Sales", () => {
     cy.get("select").select("Sales").should("have.value", "sales");
+    cy.wait("@getSales", { timeout: 10000 });
   });
 
   it("should select Subscriptions", () => {
+    cy.intercept("GET", "/api/subscriptions", {
+      statusCode: 200,
+      body: [
+        {
+          timestamp: "2020-06-17T06:44:02.676475",
+          amount: 4,
+        },
+        {
+          timestamp: "2020-06-17T06:45:30.983656",
+          amount: 2,
+        },
+        {
+          timestamp: "2020-06-18T06:45:30.983656",
+          amount: 4,
+        },
+      ],
+    }).as("getSubscriptions");
     cy.get("select")
       .select("Subscriptions")
       .should("have.value", "subscriptions");
+    cy.wait("@getSubscriptions", { timeout: 15000 });
   });
 });
